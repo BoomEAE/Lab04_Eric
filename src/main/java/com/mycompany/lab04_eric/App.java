ae5b0d432763933b161ba3efc2f67ddee60aa477
Eric <86203664+BoomEAE@users.noreply.github.com>
2026-09-14 14:40:24 -0700

    Updated the UI layout so that it's centered to the screen and resized at all times

--- src/main/java/com/mycompany/lab04_eric/App.java
+++ src/main/java/com/mycompany/lab04_eric/App.java
@@ -1,6 +1,7 @@
 package com.mycompany.lab04_eric;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
@@ -17,15 +18,12 @@ import javafx.scene.layout.BorderPane;
  * JavaFX App
  */
 public class App extends Application {
+    
+    //
     private static int numDays = 0;
-    private static int amountAirfare = 0;
-    private static int amountCarRental = 0;
-    private static int numMilesDriven = 0;
-    private static int amountParkingFees = 0;
-    private static int amountTaxiCharges = 0;
-    private static int conRegFees = 0;
-    private static int lodgingCharges = 0;
     private static int isValidCounter = 0;
+    private static double[] amounts = {0, 0, 0, 0, 0, 0, 0};
+    private static int[] countCaps = {0, 0, 0, 0, 0, 0, 0, 0};
     
     @Override
     public void start(Stage mainStage) {
@@ -33,12 +31,9 @@ public class App extends Application {
         //Creating BorderPane that will be at the root of every node
         BorderPane root = new BorderPane();
         
-        //Creating the gridpane that will grid nodes
-        GridPane gridPane = new GridPane();
-        gridPane.setVgap(10);
-        gridPane.setHgap(20);
-        
         //Creating VBoxs
+        VBox mainContainer = new VBox();
+        mainContainer.setAlignment(Pos.CENTER);
         VBox vbox1 = new VBox();
         VBox vbox2 = new VBox();
         VBox vbox3 = new VBox();
@@ -53,21 +48,21 @@ public class App extends Application {
         HBox buttonsContainer = new HBox();
         
         //Labels
-        Label numDaysLabel = new Label("Number of days on the trip");
+        Label numDaysLabel = new Label("Number of days on the trip:");
         Label validNumDaysLabel = new Label();
-        Label amoutAirfareLabel = new Label("Amount of airfare, if any");
+        Label amoutAirfareLabel = new Label("Amount of airfare, if any:");
         Label validAmoutAirfareLabel = new Label();
-        Label amoutCarRentalLabel = new Label("Amount of car rental fees, if any");
+        Label amoutCarRentalLabel = new Label("Amount of car rental fees, if any:");
         Label validAmoutCarRentalLabel = new Label();
-        Label numMilesDrivenLabel = new Label("Number of miles driven, if a private vehicle was used");
+        Label numMilesDrivenLabel = new Label("Number of miles driven, if a private vehicle was used:");
         Label validNumMilesDrivenLabel = new Label();
-        Label amountParkingLabel = new Label("Amount of parking fees, if any");
+        Label amountParkingLabel = new Label("Amount of parking fees, if any:");
         Label validAmountParkingLabel = new Label();
-        Label taxiChargesLabel = new Label("Amount of taxi charges, if any");
+        Label taxiChargesLabel = new Label("Amount of taxi charges, if any:");
         Label validTaxiChargesLabel = new Label();
-        Label conRegFeeLabel = new Label("Conference or seminar registration fees, if any");
+        Label conRegFeeLabel = new Label("Conference or seminar registration fees, if any:");
         Label validConRegFeeLabel = new Label();
-        Label lodgingChagesLabel = new Label("Lodging charges, per night");
+        Label lodgingChagesLabel = new Label("Lodging charges, per night:");
         Label validLodgingChagesLabel = new Label();
         Label totalExpensesLabel = new Label(); 
         Label totalAllowableLabel = new Label(); 
@@ -86,6 +81,8 @@ public class App extends Application {
         
         //Buttons for validation
         Button calculateExpensesBtn = new Button("calculate");
+        calculateExpensesBtn.setStyle("-fx-background-radius: 2;-fx-background-color:#FFD700;");
+        calculateExpensesBtn.setDisable(true);
         Button clearFieldsBtn = new Button("clear");
         
         //Adding appropriate nodes to their VBox
@@ -101,65 +98,56 @@ public class App extends Application {
         buttonsContainer.getChildren().addAll(calculateExpensesBtn, clearFieldsBtn);
         
         //Add to gridPane
-        gridPane.add(vbox1, 0, 0);
-        gridPane.add(vbox2, 0, 1);
-        gridPane.add(vbox3, 0, 2);
-        gridPane.add(vbox4, 0, 3);
-        gridPane.add(vbox5, 0, 4);
-        gridPane.add(vbox6, 0, 5);
-        gridPane.add(vbox7, 0, 6);
-        gridPane.add(vbox8, 0, 7);
-        gridPane.add(expensesPanel, 0, 8);
-        gridPane.add(buttonsContainer, 0, 9);
+        mainContainer.getChildren().addAll(vbox1, vbox2, vbox3, vbox4, vbox5, vbox6, vbox7, vbox8, expensesPanel, buttonsContainer);
         
         //
-        numDaysField.setOnKeyTyped(event -> ValidateNum(calculateExpensesBtn, numDaysField));
-        amoutAirfareField.setOnKeyTyped(event -> ValidateAmount(calculateExpensesBtn, amoutAirfareField));
-        amoutCarRentalField.setOnKeyTyped(event -> ValidateAmount(calculateExpensesBtn, amoutCarRentalField));
-        numMilesDrivenField.setOnKeyTyped(event -> ValidateAmount(calculateExpensesBtn, numMilesDrivenField));
-        amountParkingField.setOnKeyTyped(event -> ValidateAmount(calculateExpensesBtn, amountParkingField));
-        taxiChargesField.setOnKeyTyped(event -> ValidateAmount(calculateExpensesBtn, taxiChargesField));
-        conRegFeeFIeld.setOnKeyTyped(event -> ValidateAmount(calculateExpensesBtn, conRegFeeFIeld));
-        lodgingChagesField.setOnKeyTyped(event -> ValidateAmount(calculateExpensesBtn, lodgingChagesField));
+        numDaysField.setOnKeyTyped(event -> ValidateNum(calculateExpensesBtn, numDaysField, 0));
+        amoutAirfareField.setOnKeyTyped(event -> ValidateAmount(calculateExpensesBtn, amoutAirfareField, 1));
+        amoutCarRentalField.setOnKeyTyped(event -> ValidateAmount(calculateExpensesBtn, amoutCarRentalField, 2));
+        numMilesDrivenField.setOnKeyTyped(event -> ValidateAmount(calculateExpensesBtn, numMilesDrivenField, 3));
+        amountParkingField.setOnKeyTyped(event -> ValidateAmount(calculateExpensesBtn, amountParkingField, 4));
+        taxiChargesField.setOnKeyTyped(event -> ValidateAmount(calculateExpensesBtn, taxiChargesField, 5));
+        conRegFeeFIeld.setOnKeyTyped(event -> ValidateAmount(calculateExpensesBtn, conRegFeeFIeld, 6));
+        lodgingChagesField.setOnKeyTyped(event -> ValidateAmount(calculateExpensesBtn, lodgingChagesField, 7));
         
         //
         calculateExpensesBtn.setOnAction(event -> {
-            double totalExpenses = numDays + amountAirfare + amountCarRental+ 0.27*numMilesDriven+ amountParkingFees+ amountTaxiCharges+ conRegFees+ lodgingCharges;
+            double totalExpenses = numDays + amounts[0] + amounts[1] + 0.27*amounts[2] + amounts[3] + amounts[4] + amounts[5] + amounts[6];
             double totalAllowable = 1000.00;
             double excess = 0.0;
             double amountSaved = 0.0;
             
-            if(amountParkingFees <= numDays * 10.0){
-                amountSaved += (numDays * 10.0) - amountParkingFees;
+            if(amounts[3] <= numDays * 10.0){
+                amountSaved += (numDays * 10.0) - amounts[3];
             }else{
-                excess += amountParkingFees - (numDays * 10.0);
+                excess += amounts[3] - (numDays * 10.0);
             }
             
-            if(amountTaxiCharges <= numDays * 20.0){
-                amountSaved += (numDays * 20.0) - amountTaxiCharges;
+            if(amounts[4] <= numDays * 20.0){
+                amountSaved += (numDays * 20.0) - amounts[4];
             }else{
-                excess += amountTaxiCharges - (numDays * 20.0);
+                excess += amounts[4] - (numDays * 20.0);
             }
             
-            if(lodgingCharges <= numDays * 95.0){
-                amountSaved += (numDays * 95.0) - lodgingCharges;
+            if(amounts[6] <= numDays * 95.0){
+                amountSaved += (numDays * 95.0) - amounts[6];
             }else{
-                excess += lodgingCharges - (numDays * 95.0);
+                excess += amounts[6] - (numDays * 95.0);
             }
             
             totalExpensesLabel.setText("Total expenses: " + Double.toString(totalExpenses) + "$");
-            totalExpensesLabel.setId("label2");
+            totalExpensesLabel.setStyle("-fx-text-fill:red");
             totalAllowableLabel.setText("Total allowable: " + Double.toString(totalAllowable) + "$");
-            totalAllowableLabel.setId("label2");
+            totalAllowableLabel.setStyle("-fx-text-fill:red");
             excessLabel.setText("Total excess: " + Double.toString(excess) + "$");
-            excessLabel.setId("label2");
+            excessLabel.setStyle("-fx-text-fill:red");
             amountSavedLabel.setText("Total saved: " + Double.toString(amountSaved) + "$");
-            amountSavedLabel.setId("label2");
+            amountSavedLabel.setStyle("-fx-text-fill:red");
             
         });
         
-        root.getChildren().add(gridPane);
-        Scene scene = new Scene(root, 500, 500);
+        root.setCenter(mainContainer);
+        Scene scene = new Scene(root, 600, 800);
         mainStage.setScene(scene);
         mainStage.show();
     }
@@ -168,24 +156,42 @@ public class App extends Application {
         launch();
     }
     
-    public static void ValidateNum(Button validateBtn, TextField textField){
-        try{
+    public static void ValidateNum(Button validateBtn, TextField textField, int countCapIndex){
+        if(textField.getText().length() != 0){
+            try{
                 numDays = Integer.parseInt(textField.getText());
-                isValidCounter += 1;
+                if(countCaps[countCapIndex] != 1){
+                    isValidCounter += 1;
+                    countCaps[countCapIndex] = 1;
+                }
             }catch(NumberFormatException e){
                 System.out.println("BAD");
                 isValidCounter -= 1;
+                countCaps[countCapIndex] = 0;
             }
-            ValidateFields(validateBtn);
+        }else{
+            isValidCounter -= 1;
+            countCaps[countCapIndex] = 0;
+        }
+        ValidateFields(validateBtn);
     }
     
-    public static void ValidateAmount(Button validateBtn, TextField textField){
-        try{
-            amountAirfare = Integer.parseInt(textField.getText());
-            isValidCounter += 1;
-        }catch(NumberFormatException e){
-            System.out.println("BAD");
+    public static void ValidateAmount(Button validateBtn, TextField textField, int countCapIndex){
+        if(textField.getText().length() != 0){
+            try{
+                amounts[countCapIndex-1] = Double.parseDouble(textField.getText());
+                if(countCaps[countCapIndex] != 1){
+                    isValidCounter += 1;
+                    countCaps[countCapIndex] = 1;
+                }
+            }catch(NumberFormatException e){
+                System.out.println("BAD");
+                isValidCounter -= 1;
+                countCaps[countCapIndex] = 0;
+            }
+        }else{
             isValidCounter -= 1;
+            countCaps[countCapIndex] = 0;
         }
         ValidateFields(validateBtn);
     }
@@ -193,8 +199,10 @@ public class App extends Application {
     public static void ValidateFields(Button validateBtn){
         if(isValidCounter == 8){
             validateBtn.setDisable(false);
-        }else{
+            validateBtn.setId("btn1");
+        }else if(isValidCounter < 8){
             validateBtn.setDisable(true);
+            validateBtn.setId("btn2");
         }
     }
 
