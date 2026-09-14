d668681282d46d23c4a1270f7b87ab7e55a0da87
Eric <86203664+BoomEAE@users.noreply.github.com>
2026-09-14 09:55:53 -0700

    Reworked the layout of the UIs Removed methods that were duplicates of other methods

--- src/main/java/com/mycompany/lab04_eric/App.java
+++ src/main/java/com/mycompany/lab04_eric/App.java
@@ -7,6 +7,7 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.BorderPane;
@@ -34,26 +35,44 @@ public class App extends Application {
         
         //Creating the gridpane that will grid nodes
         GridPane gridPane = new GridPane();
+        gridPane.setVgap(10);
+        gridPane.setHgap(20);
         
-        //Creating HBoxs
-        HBox hbox1 = new HBox();
-        HBox hbox2 = new HBox();
-        HBox hbox3 = new HBox();
-        HBox hbox4 = new HBox();
-        HBox hbox5 = new HBox();
-        HBox hbox6 = new HBox();
-        HBox hbox7 = new HBox();
-        HBox hbox8 = new HBox();
+        //Creating VBoxs
+        VBox vbox1 = new VBox();
+        VBox vbox2 = new VBox();
+        VBox vbox3 = new VBox();
+        VBox vbox4 = new VBox();
+        VBox vbox5 = new VBox();
+        VBox vbox6 = new VBox();
+        VBox vbox7 = new VBox();
+        VBox vbox8 = new VBox();
+        VBox expensesPanel = new VBox();
+        
+        //Creating HBox
+        HBox buttonsContainer = new HBox();
         
         //Labels
         Label numDaysLabel = new Label("Number of days on the trip");
+        Label validNumDaysLabel = new Label();
         Label amoutAirfareLabel = new Label("Amount of airfare, if any");
+        Label validAmoutAirfareLabel = new Label();
         Label amoutCarRentalLabel = new Label("Amount of car rental fees, if any");
+        Label validAmoutCarRentalLabel = new Label();
         Label numMilesDrivenLabel = new Label("Number of miles driven, if a private vehicle was used");
+        Label validNumMilesDrivenLabel = new Label();
         Label amountParkingLabel = new Label("Amount of parking fees, if any");
+        Label validAmountParkingLabel = new Label();
         Label taxiChargesLabel = new Label("Amount of taxi charges, if any");
+        Label validTaxiChargesLabel = new Label();
         Label conRegFeeLabel = new Label("Conference or seminar registration fees, if any");
+        Label validConRegFeeLabel = new Label();
         Label lodgingChagesLabel = new Label("Lodging charges, per night");
+        Label validLodgingChagesLabel = new Label();
+        Label totalExpensesLabel = new Label(); 
+        Label totalAllowableLabel = new Label(); 
+        Label excessLabel = new Label(); 
+        Label amountSavedLabel = new Label();
         
         //TextFields
         TextField numDaysField = new TextField();
@@ -66,65 +85,49 @@ public class App extends Application {
         TextField lodgingChagesField = new TextField();
         
         //Buttons for validation
-        Button numDaysBtn = new Button("Validate");
-        Button amoutAirfareBtn = new Button("Validate");
-        Button amoutCarRentalBtn = new Button("Validate");
-        Button numMilesDrivenBtn = new Button("Validate");
-        Button amountParkingBtn = new Button("Validate");
-        Button taxiChargesBtn = new Button("Validate");
-        Button conRegFeeBtn = new Button("Validate");
-        Button lodgingChagesBtn = new Button("Validate");
-        Button validateExpenses = new Button("Validate");
-        validateExpenses.setDisable(true);
+        Button calculateExpensesBtn = new Button("calculate");
+        Button clearFieldsBtn = new Button("clear");
         
-        //
-        validateExpenses.setOnKeyTyped(event -> ValidateFields(validateExpenses));
-        
-        //Adding to hboxs
-        hbox1.getChildren().addAll(numDaysField, numDaysBtn);
-        hbox2.getChildren().addAll(amoutAirfareField, amoutAirfareBtn);
-        hbox3.getChildren().addAll(amoutCarRentalField, amoutCarRentalBtn);
-        hbox4.getChildren().addAll(numMilesDrivenField, numMilesDrivenBtn);
-        hbox5.getChildren().addAll(amountParkingField, amountParkingBtn);
-        hbox6.getChildren().addAll(taxiChargesField, taxiChargesBtn);
-        hbox7.getChildren().addAll(conRegFeeFIeld, conRegFeeBtn);
-        hbox8.getChildren().addAll(lodgingChagesField, lodgingChagesBtn);
+        //Adding appropriate nodes to their VBox
+        vbox1.getChildren().addAll(numDaysLabel, numDaysField, validNumDaysLabel);
+        vbox2.getChildren().addAll(amoutAirfareLabel, amoutAirfareField, validAmoutAirfareLabel);
+        vbox3.getChildren().addAll(amoutCarRentalLabel, validAmoutCarRentalLabel, amoutCarRentalField);
+        vbox4.getChildren().addAll(numMilesDrivenLabel, validNumMilesDrivenLabel, numMilesDrivenField);
+        vbox5.getChildren().addAll(amountParkingLabel, validAmountParkingLabel, amountParkingField);
+        vbox6.getChildren().addAll(taxiChargesLabel, validTaxiChargesLabel, taxiChargesField);
+        vbox7.getChildren().addAll(conRegFeeLabel, validConRegFeeLabel, conRegFeeFIeld);
+        vbox8.getChildren().addAll(lodgingChagesLabel, validLodgingChagesLabel, lodgingChagesField);
+        expensesPanel.getChildren().addAll(totalExpensesLabel, totalAllowableLabel, excessLabel, amountSavedLabel);
+        buttonsContainer.getChildren().addAll(calculateExpensesBtn, clearFieldsBtn);
         
         //Add to gridPane
-        gridPane.add(numDaysLabel, 0, 0);
-        gridPane.add(hbox1, 0, 1);
-        gridPane.add(amoutAirfareLabel, 0, 2);
-        gridPane.add(hbox2, 0, 3);
-        gridPane.add(amoutCarRentalLabel, 0, 4);
-        gridPane.add(hbox3, 0, 5);
-        gridPane.add(numMilesDrivenLabel, 0, 6);
-        gridPane.add(hbox4, 0, 7);
-        gridPane.add(amountParkingLabel, 0, 8);
-        gridPane.add(hbox5, 0, 9);
-        gridPane.add(taxiChargesLabel, 0, 10);
-        gridPane.add(hbox6, 0, 11);
-        gridPane.add(conRegFeeLabel, 0, 12);
-        gridPane.add(hbox7, 0, 13);
-        gridPane.add(lodgingChagesLabel, 0, 14);
-        gridPane.add(hbox8, 0, 15);
-        gridPane.add(validateExpenses, 0, 16);
+        gridPane.add(vbox1, 0, 0);
+        gridPane.add(vbox2, 0, 1);
+        gridPane.add(vbox3, 0, 2);
+        gridPane.add(vbox4, 0, 3);
+        gridPane.add(vbox5, 0, 4);
+        gridPane.add(vbox6, 0, 5);
+        gridPane.add(vbox7, 0, 6);
+        gridPane.add(vbox8, 0, 7);
+        gridPane.add(expensesPanel, 0, 8);
+        gridPane.add(buttonsContainer, 0, 9);
         
         //
-        ValidateNumDays(numDaysBtn, validateExpenses, numDaysField);
-        ValidateAmountAirfare(amoutAirfareBtn, validateExpenses, amoutAirfareField);
-        validateAmountCarRental(amoutCarRentalBtn, validateExpenses, amoutCarRentalField);
-        validateNumMilesDriven(numMilesDrivenBtn, validateExpenses, numMilesDrivenField);
-        validateAmountParking(amountParkingBtn, validateExpenses, amountParkingField);
-        validateAmountTaxiCharges(taxiChargesBtn, validateExpenses, taxiChargesField);
-        validateConRegFees(conRegFeeBtn, validateExpenses, conRegFeeFIeld);
-        validateLodgingCharges(lodgingChagesBtn, validateExpenses, lodgingChagesField);
+        numDaysField.setOnKeyTyped(event -> ValidateNum(calculateExpensesBtn, numDaysField));
+        amoutAirfareField.setOnKeyTyped(event -> ValidateAmount(calculateExpensesBtn, amoutAirfareField));
+        amoutCarRentalField.setOnKeyTyped(event -> ValidateAmount(calculateExpensesBtn, amoutCarRentalField));
+        numMilesDrivenField.setOnKeyTyped(event -> ValidateAmount(calculateExpensesBtn, numMilesDrivenField));
+        amountParkingField.setOnKeyTyped(event -> ValidateAmount(calculateExpensesBtn, amountParkingField));
+        taxiChargesField.setOnKeyTyped(event -> ValidateAmount(calculateExpensesBtn, taxiChargesField));
+        conRegFeeFIeld.setOnKeyTyped(event -> ValidateAmount(calculateExpensesBtn, conRegFeeFIeld));
+        lodgingChagesField.setOnKeyTyped(event -> ValidateAmount(calculateExpensesBtn, lodgingChagesField));
         
         //
-        validateExpenses.setOnAction(event -> {
+        calculateExpensesBtn.setOnAction(event -> {
             double totalExpenses = numDays + amountAirfare + amountCarRental+ 0.27*numMilesDriven+ amountParkingFees+ amountTaxiCharges+ conRegFees+ lodgingCharges;
             double totalAllowable = 1000.00;
-            double excess = 0;
-            double amountSaved = 0;
+            double excess = 0.0;
+            double amountSaved = 0.0;
             
             if(amountParkingFees <= numDays * 10.0){
                 amountSaved += (numDays * 10.0) - amountParkingFees;
@@ -144,10 +147,14 @@ public class App extends Application {
                 excess += lodgingCharges - (numDays * 95.0);
             }
             
-            System.out.println(totalExpenses);
-            System.out.println(totalAllowable);
-            System.out.println(excess);
-            System.out.println(amountSaved);
+            totalExpensesLabel.setText("Total expenses: " + Double.toString(totalExpenses) + "$");
+            totalExpensesLabel.setId("label2");
+            totalAllowableLabel.setText("Total allowable: " + Double.toString(totalAllowable) + "$");
+            totalAllowableLabel.setId("label2");
+            excessLabel.setText("Total excess: " + Double.toString(excess) + "$");
+            excessLabel.setId("label2");
+            amountSavedLabel.setText("Total saved: " + Double.toString(amountSaved) + "$");
+            amountSavedLabel.setId("label2");
             
         });
         
@@ -161,9 +168,8 @@ public class App extends Application {
         launch();
     }
     
-    public static void ValidateNumDays(Button validateFeldBtn, Button validateBtn, TextField textField){
-        validateFeldBtn.setOnAction(event -> {
-            try{
+    public static void ValidateNum(Button validateBtn, TextField textField){
+        try{
                 numDays = Integer.parseInt(textField.getText());
                 isValidCounter += 1;
             }catch(NumberFormatException e){
@@ -171,98 +177,17 @@ public class App extends Application {
                 isValidCounter -= 1;
             }
             ValidateFields(validateBtn);
-        });
     }
     
-    public static void ValidateAmountAirfare(Button validateFeldBtn, Button validateBtn, TextField textField){
-        validateFeldBtn.setOnAction(event -> {
-            try{
-                amountAirfare = Integer.parseInt(textField.getText());
-                isValidCounter += 1;
-            }catch(NumberFormatException e){
-                System.out.println("BAD");
-                isValidCounter -= 1;
-            }
-            ValidateFields(validateBtn);
-        });
-    }
-    
-    public static void validateAmountCarRental(Button validateFeldBtn, Button validateBtn, TextField textField){
-        validateFeldBtn.setOnAction(event -> {
-            try{
-                amountCarRental = Integer.parseInt(textField.getText());
-                isValidCounter += 1;
-            }catch(NumberFormatException e){
-                System.out.println("BAD");
-                isValidCounter -= 1;
-            }
-            ValidateFields(validateBtn);
-        });
-    }
-    
-    public static void validateNumMilesDriven(Button validateFeldBtn, Button validateBtn, TextField textField){
-        validateFeldBtn.setOnAction(event -> {
-            try{
-                numMilesDriven = Integer.parseInt(textField.getText());
-                isValidCounter += 1;
-            }catch(NumberFormatException e){
-                System.out.println("BAD");
-                isValidCounter -= 1;
-            }
-            ValidateFields(validateBtn);
-        });
-    }
-    
-    public static void validateAmountParking(Button validateFeldBtn, Button validateBtn, TextField textField){
-        validateFeldBtn.setOnAction(event -> {
-            try{
-                amountParkingFees = Integer.parseInt(textField.getText());
-                isValidCounter += 1;
-            }catch(NumberFormatException e){
-                System.out.println("BAD");
-                isValidCounter -= 1;
-            }
-            ValidateFields(validateBtn);
-        });
-    }
-    
-    public static void validateAmountTaxiCharges(Button validateFeldBtn, Button validateBtn, TextField textField){
-        validateFeldBtn.setOnAction(event -> {
-            try{
-                amountTaxiCharges = Integer.parseInt(textField.getText());
-                isValidCounter += 1;
-            }catch(NumberFormatException e){
-                System.out.println("BAD");
-                isValidCounter -= 1;
-            }
-            ValidateFields(validateBtn);
-        });
-    }
-    
-    public static void validateConRegFees(Button validateFeldBtn, Button validateBtn, TextField textField){
-        validateFeldBtn.setOnAction(event -> {
-            try{
-                conRegFees = Integer.parseInt(textField.getText());
-                isValidCounter += 1;
-            }catch(NumberFormatException e){
-                System.out.println("BAD");
-                isValidCounter -= 1;
-            }
-            ValidateFields(validateBtn);
-        });
-    }
-    
-    public static void validateLodgingCharges(Button validateFeldBtn, Button validateBtn, TextField textField){
-        validateFeldBtn.setOnAction(event -> {
-            try{
-                lodgingCharges = Integer.parseInt(textField.getText());
-                isValidCounter += 1;
-            }catch(NumberFormatException e){
-                System.out.println("BAD");
-                isValidCounter -= 1;
-            }
-            ValidateFields(validateBtn);
-        });
+    public static void ValidateAmount(Button validateBtn, TextField textField){
+        try{
+            amountAirfare = Integer.parseInt(textField.getText());
+            isValidCounter += 1;
+        }catch(NumberFormatException e){
+            System.out.println("BAD");
+            isValidCounter -= 1;
+        }
+        ValidateFields(validateBtn);
     }
     
     public static void ValidateFields(Button validateBtn){
